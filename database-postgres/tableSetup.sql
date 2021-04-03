ef10a45154221aeb201f0a63b39374af427f81d3
Sam Cox <sjamcox@gmail.com>
2021-04-03 11:37:17 -0700

    Add double quotes to column names to maintain camel case

--- database-postgres/tableSetup.sql
+++ database-postgres/tableSetup.sql
@@ -1,35 +1,35 @@
 DROP TABLE IF EXISTS reviews, products, customers;
 
 CREATE TABLE products (
-  productId INTEGER NOT NULL,
-  color TEXT NOT NULL,
+  "productId" INTEGER NOT NULL,
+  "color" TEXT NOT NULL,
   "configuration" TEXT NOT NULL,
-  isBestSeller BOOLEAN NOT NULL,
-  category TEXT NOT NULL
+  "isBestSeller" BOOLEAN NOT NULL,
+  "category" TEXT NOT NULL
 );
 
 CREATE TABLE customers (
-  customerId INTEGER NOT NULL,
-  customerName TEXT NOT NULL,
-  customerCountry TEXT NOT NULL,
-  profilePicUrl TEXT NOT NULL
+  "customerId" INTEGER NOT NULL,
+  "customerName" TEXT NOT NULL,
+  "customerCountry" TEXT NOT NULL,
+  "profilePicUrl" TEXT NOT NULL
 );
 
 CREATE TABLE reviews (
-  reviewId INTEGER NOT NULL,
-  title TEXT NOT NULL,
+  "reviewId" TEXT NOT NULL,
+  "title" TEXT NOT NULL,
   "description" TEXT NOT NULL,
-  rating INTEGER NOT NULL,
-  isVerifiedPurchase BOOLEAN NOT NULL,
-  isHelpfulCount INTEGER NOT NULL,
-  reviewDate DATE NOT NULL,
-  easeToUse FLOAT NOT NULL,
-  voiceRecognition FLOAT NOT NULL,
-  techSupport FLOAT NOT NULL,
-  valueForMoney FLOAT NOT NULL,
-  qualityOfMaterial FLOAT NOT NULL,
-  batteryLife FLOAT NOT NULL,
-  imageUrls TEXT[],
-  productId INTEGER,
-  customerId INTEGER
+  "rating" INTEGER NOT NULL,
+  "isVerifiedPurchase" BOOLEAN NOT NULL,
+  "isHelpfulCount" INTEGER NOT NULL,
+  "reviewDate" DATE NOT NULL,
+  "easeToUse" FLOAT NOT NULL,
+  "voiceRecognition" FLOAT NOT NULL,
+  "techSupport" FLOAT NOT NULL,
+  "valueForMoney" FLOAT NOT NULL,
+  "qualityOfMaterial" FLOAT NOT NULL,
+  "batteryLife" FLOAT NOT NULL,
+  "imageUrls" TEXT[],
+  "productId" INTEGER,
+  "customerId" INTEGER
 );
