ebf0fd2d3f4196f810ca4fdc23b77c0b35a66d92
Sam Cox <sjamcox@gmail.com>
2021-04-03 11:38:08 -0700

    Create CRUD functions for reviews table

--- database-postgres/dbHelpers.js
+++ database-postgres/dbHelpers.js
@@ -1,3 +1,4 @@
+const faker = require('faker');
 const { Client } = require('pg');
 const client = new Client({
   database: 'sdc'
@@ -7,6 +8,14 @@ client
   .connect()
   .then(() => console.log('connected to Postgres'))
   .catch(err => console.error('connection error', err.stack))
+
+const formatValue = (value) => {
+  if (Array.isArray(value)) {
+    return `ARRAY${JSON.stringify(value)}`
+  } else {
+    return JSON.stringify(value)
+  }
+}
 
 exports.getReviewsByProductId = async (id) => {
   const query = `
@@ -40,5 +49,38 @@ exports.getReviewsByProductId = async (id) => {
     WHERE products.productId = ${id};
   `;
   const response = await client.query(query);
-  return response;
+  return response.rows;
 };
+
+exports.getReview = (id) => {
+  const query = `SELECT * FROM reviews WHERE reviewId = ${id}`
+  return client.query(query);
+}
+
+exports.createReview = (body) => {
+  body.reviewId = faker.random.uuid();
+  const keys = Object.keys(body)
+  const columns = keys.join(',')
+  const values = keys.map(key => formatValue(body[key])).join(',').replace(/\"/g,`'`);
+  const query = `
+    INSERT INTO reviews (${columns})
+    VALUES (${values})
+  `
+  return client.query(query);
+}
+
+exports.updateReview = (id, data) => {
+  const columns = Object.keys(data);
+  const updates = columns.map(column => `${column} = ${formatValue(data[column])}`).join(', ').replace(/\"/g,`'`);
+  const query = `
+    UPDATE reviews
+    SET ${updates}
+    WHERE reviewId = ${id}
+  `
+  return client.query(query);
+}
+
+exports.deleteReview = (id) => {
+  const query = `DELETE FROM reviews WHERE reviewId = ${id}`
+  return client.query(query);
+}
