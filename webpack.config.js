c4a8ebcf743695440ef885a0c753c9643a13c317
Sam Cox <sjamcox@gmail.com>
2021-04-06 20:14:06 -0700

    Specify production mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
+  mode: 'production',
   entry: `${SRC_DIR}/index.jsx`,
   module: {
     rules: [
