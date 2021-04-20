09470f9e6bfff6a9861f717fc2dbf64168dd52af
EC2 Default User <ec2-user@ip-172-31-21-255.us-west-2.compute.internal>
2021-04-20 06:18:57 +0000

    Perf updates

--- server/app.js
+++ server/app.js
@@ -7,14 +7,11 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 app.use(cors());
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(express.static(__dirname + '/../public'));
-app.use(express.static(__dirname + '/../public/dist'));
-app.use(express.static(__dirname + '/../client'));
 app.use('/:id', express.static(__dirname + '/../public/dist'));
 app.use('/:id', express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/Reviews/getReviews/:productId', async (req, res) => {
   try {
